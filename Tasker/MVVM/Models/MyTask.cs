11f74d8fe1b3672f64a6ed0e90a177b846dc1ab6
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2023-04-24 01:49:06 -0500

    Seccion Tareas Pendientes

--- Tasker/MVVM/Models/MyTask.cs
+++ Tasker/MVVM/Models/MyTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Tasker.MVVM.Models
 {
+    [AddINotifyPropertyChangedInterface]
     public class MyTask
     {
         public string TaskName { get; set; }
--- Tasker/MVVM/Views/MainView.xaml.cs
+++ Tasker/MVVM/Views/MainView.xaml.cs
@@ -4,9 +4,15 @@ namespace Tasker.MVVM.Views;
 
 public partial class MainView : ContentPage
 {
+	private MainViewModel mainViewModel = new MainViewModel();
 	public MainView()
 	{
 		InitializeComponent();
-		BindingContext = new MainViewModel();
+		BindingContext = mainViewModel;
+	}
+
+	private void checkBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+	{
+		mainViewModel.UpdateData();
 	}
 }
