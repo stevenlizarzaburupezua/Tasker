943819241474fe5c79d93f4d8c3f2949c708a427
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2023-04-23 23:23:06 -0500

    Seccion Categorias completo

--- Tasker/MVVM/ViewModel/MainViewModel.cs
+++ Tasker/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -8,6 +9,7 @@ using Tasker.MVVM.Models;
 
 namespace Tasker.MVVM.ViewModel
 {
+    [AddINotifyPropertyChangedInterface]
     public class MainViewModel
     {
         public ObservableCollection<Category> Categories { get; set; }
@@ -22,56 +24,72 @@ namespace Tasker.MVVM.ViewModel
         private void FillData()
         {
             Categories = new ObservableCollection<Category>
-            {
-                new Category
-                {
-                    Id = 1,
-                    CategoryName = ".NET MAUI Course",
-                    Color = "#CF14DF"
-                },
-                new Category
-                {
-                    Id = 2,
-                    CategoryName = "Tutorials",
-                    Color = "#df6f14"
-                },
-                new Category
-                {
-                    Id = 3,
-                    CategoryName = "Shopping",
-                    Color = "#14df80"
-                },
-            };
+               {
+                    new Category
+                    {
+                         Id = 1,
+                         CategoryName = ".NET MAUI Course",
+                         Color = "#CF14DF"
+                    },
+                    new Category
+                    {
+                         Id = 2,
+                         CategoryName = "Tutorials",
+                         Color = "#df6f14"
+                    },
+                    new Category
+                    {
+                         Id = 3,
+                         CategoryName = "Shopping",
+                         Color = "#14df80"
+                    }
+               };
 
             Tasks = new ObservableCollection<MyTask>
-            {
-                new MyTask
-                {
-                    TaskName = "Upload Exercise Files",
-                    Completed = false,
-                    CategoryId = 1
-                },
-                new MyTask
-                {
-                    TaskName = "Plan Next Course",
-                    Completed = false,
-                    CategoryId = 1
-                },
-                new MyTask
-                {
-                    TaskName = "Upload new ASP.NET video on Youtube",
-                    Completed = false,
-                    CategoryId = 2
-                },
-
-                new MyTask
-                {
-                    TaskName = "Fix Settings.cs class of the project",
-                    Completed = false,
-                    CategoryId = 2
-                },
-
-            };
+               {
+                    new MyTask
+                    {
+                         TaskName = "Upload exercise files",
+                         Completed = false,
+                         CategoryId = 1
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Plan next course",
+                         Completed = false,
+                         CategoryId = 1
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Upload new ASP.NET video on YouTube",
+                         Completed = false,
+                         CategoryId = 2
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Fix Settings.cs class of the project",
+                         Completed = false,
+                         CategoryId = 2
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Update github repository",
+                         Completed = true,
+                         CategoryId = 2
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Buy eggs",
+                         Completed = false,
+                         CategoryId = 3
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Go for the pepperoni pizza",
+                         Completed = false,
+                         CategoryId = 3
+                    },
+               };
 
             UpdateData();
 
@@ -103,7 +121,7 @@ namespace Tasker.MVVM.ViewModel
                 var catColor = (from c in Categories
                                 where c.Id == t.CategoryId
                                 select c.Color).FirstOrDefault();
-                t.TaskName = catColor;
+                t.TaskColor = catColor;
             }
 
         }
